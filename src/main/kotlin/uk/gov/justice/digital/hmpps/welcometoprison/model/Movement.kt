74596e408240e1416ba198592f10ed8ecbcd0d43
Andrew Lee <1517745+andrewrlee@users.noreply.github.com>
2021-08-31 15:55:07 +0100

    DCS-1165 Fixing swagger doc/deployment (#7)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/welcometoprison/model/Movement.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/welcometoprison/model/Movement.kt
@@ -1,14 +1,26 @@
 package uk.gov.justice.digital.hmpps.welcometoprison.model
 
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL
+import io.swagger.v3.oas.annotations.media.Schema
 import java.time.LocalDate
 
+@JsonInclude(NON_NULL)
+@Schema(description = "A movement into prison")
 data class Movement(
+  @Schema(description = "First name", example = "Sam")
   val firstName: String,
+  @Schema(description = "Last name", example = "Smith")
   val lastName: String,
+  @Schema(description = "Date of birth", example = "1971-02-01")
   val dateOfBirth: LocalDate,
+  @Schema(description = "Prison number", example = "A1234AA")
   val prisonNumber: String,
+  @Schema(description = "PNC number", example = "01/1234X")
   val pncNumber: String,
+  @Schema(description = "Date expected", example = "2020-02-23")
   val date: LocalDate,
+  @Schema(description = "Move type", example = "Crown House")
   val moveType: MoveType
 )
 
