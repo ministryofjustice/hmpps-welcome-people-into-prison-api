4e7dfe4fa8eb2a1e03c8d0ec722c0dfdba477bdc
andrewrlee <1517745+andrewrlee@users.noreply.github.com>
2021-09-06 08:59:54 +0100

    DCS-1165 Adding JSON-API query builder

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/welcometoprison/model/basm/JsonApiQueryBuilder.kt
@@ -0,0 +1,34 @@
+package uk.gov.justice.digital.hmpps.welcometoprison.model.basm
+
+import java.net.URLEncoder
+
+typealias Param<T> = Pair<String, T>
+
+object JsonApiQueryBuilder {
+  enum class Order { asc, desc }
+
+  fun `query of`(
+    includes: List<String>? = null,
+    filters: Map<String, List<String>>? = null,
+    sort: Pair<String, Order>? = null,
+    page: Int? = null,
+    perPage: Int? = null
+  ): String {
+
+    val includeParams = includes.toParams { listOf("include" to it.joinToString(",")) }
+    val perPageParam = perPage.toParams { listOf("per_page" to it) }
+    val pageParam = page.toParams { listOf("page" to it) }
+    val sortParam = sort.toParams { (name, order) -> listOf("sort[by]" to name, "sort[direction]" to order) }
+    val filterParams =
+      filters.toParams { it.toList().map { (name, values) -> "filter[$name]" to values.joinToString(",") } }
+
+    val params = arrayOf(includeParams, filterParams, pageParam, perPageParam, sortParam).flatten()
+    return if (params.isNotEmpty()) "?${params.joinToString("&")}" else ""
+  }
+
+  private fun <T, R> T?.toParams(build: (value: T) -> List<Param<R>>) =
+    if (this == null) emptyArray() else build(this).map { it.encoded() }.toTypedArray()
+
+  private fun <T> Param<T>.encoded() = "${first.encoded()}=${second.toString().encoded()}"
+  private fun String.encoded() = URLEncoder.encode(this, "utf-8")
+}
