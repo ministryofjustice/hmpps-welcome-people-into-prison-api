3b7cc5669adcd3d6fba18b5961b08e154188d60e
GCheema <50441412+GurnankCheema@users.noreply.github.com>
2021-09-20 09:42:23 +0100

    DCS-1192: generate fake data for non-BaSM prison transfers (#14)
    * DCS-1192: generate fake data for non-BaSM prison transfers
    
    * DCS-1192 fix linting errors
    
    * DCS-1192 code refactor
    Moved the getMoves() function from its own class into the model/prison/PrisonService class

--- src/main/kotlin/uk/gov/justice/digital/hmpps/welcometoprison/model/MovementService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/welcometoprison/model/MovementService.kt
@@ -2,9 +2,11 @@ package uk.gov.justice.digital.hmpps.welcometoprison.model
 
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.welcometoprison.model.basm.BasmService
+import uk.gov.justice.digital.hmpps.welcometoprison.model.prison.PrisonService
 import java.time.LocalDate
 
 @Service
-class MovementService(val basmService: BasmService) {
-  fun getMovements(agencyId: String, date: LocalDate) = basmService.getMoves(agencyId, date, date)
+class MovementService(val basmService: BasmService, val prisonService: PrisonService) {
+  fun getMovements(agencyId: String, date: LocalDate) =
+    basmService.getMoves(agencyId, date, date) + prisonService.getMoves(agencyId, date)
 }
--- src/main/kotlin/uk/gov/justice/digital/hmpps/welcometoprison/model/prison/PrisonService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/welcometoprison/model/prison/PrisonService.kt
@@ -1,11 +1,17 @@
 package uk.gov.justice.digital.hmpps.welcometoprison.model.prison
 
+import com.github.javafaker.Faker
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.welcometoprison.exception.NotFoundException
+import uk.gov.justice.digital.hmpps.welcometoprison.model.MoveType
+import uk.gov.justice.digital.hmpps.welcometoprison.model.Movement
+import java.time.LocalDate
+import java.time.ZoneId
+import java.util.Random
 
 @Service
-class PrisonService(@Autowired private val client: PrisonApiClient) {
+class PrisonService(@Autowired private val client: PrisonApiClient, val faker: Faker = Faker()) {
 
   fun getPrisonerImage(offenderNumber: String): ByteArray? {
     val prisonerImage: PrisonerImage? =
@@ -16,4 +22,21 @@ class PrisonService(@Autowired private val client: PrisonApiClient) {
     return prisonerImage?.let { client.getPrisonerImage(it.imageId) }
       ?: throw NotFoundException("No front-facing image of the offenders face found for offender number: $offenderNumber")
   }
+
+  private val numbers = '0'..'9'
+  private val letters = 'A'..'Z'
+  fun CharRange.get(n: Int) = this.shuffled().take(n).joinToString("")
+
+  fun getMoves(agencyId: String, date: LocalDate) = generateSequence {
+    Movement(
+      firstName = faker.name().firstName(),
+      lastName = faker.name().lastName(),
+      dateOfBirth = faker.date().birthday().toInstant().atZone(ZoneId.systemDefault()).toLocalDate(),
+      fromLocation = faker.address().city(),
+      prisonNumber = if (Random().nextBoolean()) letters.get(1) + letters.get(4) + numbers.get(2) else null,
+      pncNumber = numbers.get(4) + "/" + numbers.get(7) + letters.get(1),
+      date = date,
+      moveType = MoveType.PRISON_TRANSFER,
+    )
+  }.take((5..20).random()).toList()
 }
