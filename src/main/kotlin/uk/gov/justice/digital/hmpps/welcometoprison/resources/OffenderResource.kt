d5e4afb0c7ef8300dab19d2f200bd34775ae1ccf
andrewrlee <1517745+andrewrlee@users.noreply.github.com>
2021-10-18 15:56:59 +0100

    DCS-1217 Fixing linter

--- src/main/kotlin/uk/gov/justice/digital/hmpps/welcometoprison/resources/OffenderResource.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/welcometoprison/resources/OffenderResource.kt
@@ -1,7 +1,6 @@
 package uk.gov.justice.digital.hmpps.welcometoprison.resources
 
 import org.springframework.http.MediaType
-import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
