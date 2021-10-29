20d849202fe57d9a4db3cd7134f9fe731c7b369e
bell-pa <peter.bell@digital.justice.gov.uk>
2021-10-29 10:54:50 +0100

    DCS-1258: Accept arrival with existing prison record and no current booking

--- src/main/kotlin/uk/gov/justice/digital/hmpps/welcometoprison/model/confirmedarrival/ConfirmedArrivalService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/welcometoprison/model/confirmedarrival/ConfirmedArrivalService.kt
@@ -3,6 +3,7 @@ package uk.gov.justice.digital.hmpps.welcometoprison.model.confirmedarrival
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.welcometoprison.model.arrival.Arrival
 import java.time.LocalDate
+import java.time.LocalDateTime
 
 @Service
 class ConfirmedArrivalService(
@@ -15,5 +16,26 @@ class ConfirmedArrivalService(
 
   private fun contains(arrival: Arrival, confirmedArrivals: List<ConfirmedArrival>) = confirmedArrivals.stream().anyMatch {
     arrival.id == it.movementId
+  }
+
+  fun save(
+    movementId: String,
+    prisonNumber: String,
+    prisonId: String,
+    bookingId: Int,
+    arrivalDate: LocalDate,
+    arrivalType: ArrivalType
+  ): ConfirmedArrival {
+    val confirmedArrival = ConfirmedArrival(
+      null,
+      prisonNumber,
+      movementId,
+      LocalDateTime.now(),
+      arrivalType,
+      prisonId,
+      bookingId,
+      arrivalDate
+    )
+    return confirmedArrivalRepository.save(confirmedArrival)
   }
 }
