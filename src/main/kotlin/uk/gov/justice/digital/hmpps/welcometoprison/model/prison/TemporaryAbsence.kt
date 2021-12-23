ad9695bda1f33bd7964fd75cb0c7456e9af9ccd4
Connor Glynn <connor.glynn@digital.justice.gov.uk>
2021-12-23 10:44:51 +0000

    ✨DCS-1168: use @JsonAlias to only target deserialization on property

--- src/main/kotlin/uk/gov/justice/digital/hmpps/welcometoprison/model/prison/TemporaryAbsence.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/welcometoprison/model/prison/TemporaryAbsence.kt
@@ -1,8 +1,8 @@
 package uk.gov.justice.digital.hmpps.welcometoprison.model.prison
 
+import com.fasterxml.jackson.annotation.JsonAlias
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL
-import com.fasterxml.jackson.annotation.JsonProperty
 import io.swagger.v3.oas.annotations.media.Schema
 import java.time.LocalDate
 
@@ -15,10 +15,10 @@ data class TemporaryAbsence(
   val lastName: String,
   @Schema(description = "Date of birth", example = "1971-02-01")
   val dateOfBirth: LocalDate,
-  @JsonProperty("offenderNo")
+  @JsonAlias("offenderNo")
   @Schema(description = "Prison number", example = "A1234AA")
   val prisonNumber: String?,
-  @JsonProperty("movementReason")
+  @JsonAlias("movementReason")
   @Schema(description = "Reason", example = "Medical/Dental Outpatient Appointment")
   val reasonForAbsence: String
 )
