486cb2a805361b28a554c16092f3f55e81efbdcf
michalnawrockidj <92578842+michalnawrockidj@users.noreply.github.com>
2022-01-31 13:56:20 +0000

    DCS-1429 library upgrade (#89)
    * DCS-1429 library update
    
    * DCS-1429 switching library for WordUtils

--- src/main/kotlin/uk/gov/justice/digital/hmpps/welcometoprison/model/prison/Name.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/welcometoprison/model/prison/Name.kt
@@ -1,6 +1,6 @@
 package uk.gov.justice.digital.hmpps.welcometoprison.model.prison
 
-import org.apache.commons.lang3.text.WordUtils
+import org.apache.commons.text.WordUtils
 
 object Name {
   fun properCase(name: String) = WordUtils.capitalizeFully(name, '-', '\'', ' ', ',')
