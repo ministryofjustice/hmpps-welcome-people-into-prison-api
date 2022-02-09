cfcd45e878eefe87781260279714fc447830f9da
Michal Nawrocki <michal.nawrocki@digital.justice.gov.uk>
2022-02-09 11:56:25 +0000

    code review changes

--- src/main/kotlin/uk/gov/justice/digital/hmpps/welcometoprison/model/prison/courtreturns/ConfirmCourtReturnResponse.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/welcometoprison/model/prison/courtreturns/ConfirmCourtReturnResponse.kt
@@ -18,9 +18,4 @@ data class ConfirmCourtReturnResponse(
   fun getOffenderNo(): String {
     return this.prisonNumber
   }
-  @Deprecated("Please use prisonNumber instead", replaceWith = ReplaceWith("prisonNumber"))
-  @Schema(description = "prison number", example = "G6081VQ")
-  fun getPrisonerNumber(): String {
-    return this.prisonNumber
-  }
 }
