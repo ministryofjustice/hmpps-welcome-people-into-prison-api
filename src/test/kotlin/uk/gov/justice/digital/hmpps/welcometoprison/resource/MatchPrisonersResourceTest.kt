583c43daa0c2581682509446af4568eac7b7a86b
Michal Nawrocki <michal.nawrocki@digital.justice.gov.uk>
2022-02-17 16:43:38 +0000

    DCS-1449 adding tests

--- src/test/kotlin/uk/gov/justice/digital/hmpps/welcometoprison/resource/MatchPrisonersResourceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/welcometoprison/resource/MatchPrisonersResourceTest.kt
@@ -3,6 +3,7 @@ package uk.gov.justice.digital.hmpps.welcometoprison.resource
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.RepeatedTest
 import org.junit.jupiter.api.RepetitionInfo
+import org.junit.jupiter.api.Test
 import org.springframework.http.MediaType
 import uk.gov.justice.digital.hmpps.welcometoprison.integration.IntegrationTestBase
 import uk.gov.justice.digital.hmpps.welcometoprison.model.arrivals.PotentialMatch
@@ -35,5 +36,30 @@ class MatchPrisonersResourceTest : IntegrationTestBase() {
       .expectBodyList(PotentialMatch::class.java).returnResult().responseBody
 
     assertThat(resp.size).isEqualTo(repetitionInfo.currentRepetition - 1)
+  }
+  @Test
+  fun `search result with 0 matches`() {
+
+    val validRequest = """
+        {
+          "firstName": "Alpha",
+          "lastName": "Omega",
+          "dateOfBirth": "1961-05-29"
+        }
+      """
+
+    val token = getAuthorisation(roles = listOf("ROLE_VIEW_ARRIVALS"), scopes = listOf("read"))
+
+    val resp = webTestClient
+      .post()
+      .uri("/match-prisoners")
+      .withBearerToken(token)
+      .header("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+      .bodyValue(validRequest)
+      .exchange()
+      .expectStatus().isOk
+      .expectBodyList(PotentialMatch::class.java).returnResult().responseBody
+
+    assertThat(resp.size).isEqualTo(0)
   }
 }
