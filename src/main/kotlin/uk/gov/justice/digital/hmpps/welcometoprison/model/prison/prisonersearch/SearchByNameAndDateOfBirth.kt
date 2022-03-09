009e14d6b31bff1219e4a9e34e529006d1f55fa7
Michal Nawrocki <michal.nawrocki@digital.justice.gov.uk>
2022-03-09 15:47:15 +0000

    code review changes

--- src/main/kotlin/uk/gov/justice/digital/hmpps/welcometoprison/model/prison/prisonersearch/SearchByNameAndDateOfBirth.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/welcometoprison/model/prison/prisonersearch/SearchByNameAndDateOfBirth.kt
@@ -5,7 +5,7 @@ import java.time.LocalDate
 data class SearchByNameAndDateOfBirth(
   val firstName: String,
   val lastName: String,
-  val dateOfBirth: LocalDate? = null,
+  val dateOfBirth: LocalDate,
   val pncNumber: String? = null,
   val croNumber: String? = null,
   val nomsNumber: String? = null
