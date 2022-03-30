08e0342c46e481dff21368d9867188ab4660fac6
Andrew Lee <1517745+andrewrlee@users.noreply.github.com>
2022-03-30 07:49:50 +0100

    DCS-1534 Supporting search scenarios (#116)
    supporting:
    * Prison number
    * Pnc number
    * Last name and dob (with optional first name)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/welcometoprison/model/prison/prisonersearch/request/MatchPrisonersRequest.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/welcometoprison/model/prison/prisonersearch/request/MatchPrisonersRequest.kt
@@ -3,25 +3,18 @@ package uk.gov.justice.digital.hmpps.welcometoprison.model.prison.prisonersearch
 import com.fasterxml.jackson.annotation.JsonInclude
 import io.swagger.v3.oas.annotations.media.Schema
 import java.time.LocalDate
-import javax.validation.constraints.NotNull
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Schema(description = "Data for creating a search request to find matching prisoners")
 data class MatchPrisonersRequest(
 
   @Schema(description = "First name", example = "Sam")
-  @field:NotNull
-  val firstName: String,
+  val firstName: String?,
 
   @Schema(description = "Last name", example = "Smith")
-  @field:NotNull
-  val lastName: String,
+  val lastName: String?,
 
-  @Schema(
-    description = "Date of birth",
-    example = "1971-02-01"
-  )
-  @field:NotNull
+  @Schema(description = "Date of birth", example = "1971-02-01")
   val dateOfBirth: LocalDate?,
 
   @Schema(description = "Prison number", example = "A1234AA")
