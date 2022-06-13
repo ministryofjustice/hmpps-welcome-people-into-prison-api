96669a9ad96e036a723fede00d5c9e9b76ea1ee5
andrewrlee <1517745+andrewrlee@users.noreply.github.com>
2022-06-13 16:29:18 +0100

    DCS-1664 Bumping spring boot plugin to 4.2.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.2.2"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.2.3"
   kotlin("plugin.spring") version "1.6.21"
   kotlin("plugin.jpa") version "1.6.21"
 }
