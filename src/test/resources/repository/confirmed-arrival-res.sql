f23df91907c16b10f3b356d47f245b3360847d5d
michalnawrockidj <92578842+michalnawrockidj@users.noreply.github.com>
2022-07-26 15:12:54 +0100

    DCS-1720 user can be null value (#156)
    * DCS-1720 user can be null value

--- src/test/resources/repository/confirmed-arrival-res.sql
+++ src/test/resources/repository/confirmed-arrival-res.sql
@@ -1,3 +1,4 @@
 INSERT INTO confirmed_arrival (id, prison_number, movement_id, timestamp, prison_id, booking_id, arrival_date, arrival_type, username) VALUES
 (9,'A7818DY', '69c3b633-75c3-4e86-80bf-44daa6e6b9f3',to_timestamp('2020-01-10 01:01:01', 'YYYY-MM-DD HH:MI:SS'), 'MIK', 126, '2020-01-10', 'NEW_TO_PRISON', 'User U'),
-(10,'A7819DY', '65c3b633-75c3-4e86-80bf-44daa6e6b9f3',to_timestamp('2020-01-10 01:01:01', 'YYYY-MM-DD HH:MI:SS'), 'MIK', 127, '2020-01-10', 'NEW_BOOKING_EXISTING_OFFENDER', 'Mr X');
+(10,'A7819DY', '65c3b633-75c3-4e86-80bf-44daa6e6b9f3',to_timestamp('2020-01-10 01:01:01', 'YYYY-MM-DD HH:MI:SS'), 'MIK', 127, '2020-01-10', 'NEW_BOOKING_EXISTING_OFFENDER', 'Mr X'),
+(11,'A7819DY', '65c3b633-75c3-4e86-80bf-44daa6e6b9f3',to_timestamp('2020-01-10 01:01:01', 'YYYY-MM-DD HH:MI:SS'), 'MIK', 127, '2020-01-10', 'NEW_BOOKING_EXISTING_OFFENDER', NULL);
