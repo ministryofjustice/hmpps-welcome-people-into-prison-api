225d21db041c127609fcef04a4d2c62df5fa9d8a
Michal Nawrocki <michal.nawrocki@digital.justice.gov.uk>
2022-08-05 08:52:44 +0100

    DCS-1725 code review changes

--- src/main/kotlin/uk/gov/justice/digital/hmpps/bodyscan/apiclient/model/PersonalCareNeeds.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/bodyscan/apiclient/model/PersonalCareNeeds.kt
@@ -31,5 +31,5 @@ data class PersonalCareNeeds(
   fun getEndDate() = date
 
   @JsonProperty("commentText")
-  fun getCommentText() = ("$bodyScanReason-$bodyScanResult").lowercase().replaceFirstChar { it.uppercase() }
+  fun getCommentText() = ("$bodyScanReason - $bodyScanResult").lowercase().replaceFirstChar { it.uppercase() }
 }
