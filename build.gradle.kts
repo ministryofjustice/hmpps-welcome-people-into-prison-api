0e7d3bea7727dbdda7a711ce6cc06b44c58cb08d
Michal Nawrocki <michal.nawrocki@digital.justice.gov.uk>
2023-01-09 14:39:50 +0000

    security update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.7.2"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.8.0"
   kotlin("plugin.spring") version "1.7.22"
   kotlin("plugin.jpa") version "1.7.22"
 }
