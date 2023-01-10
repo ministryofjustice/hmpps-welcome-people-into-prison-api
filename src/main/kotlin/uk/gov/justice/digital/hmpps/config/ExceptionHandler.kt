d1e205e1895879891a93195b2f807816a48b377f
Michal Nawrocki <michal.nawrocki@digital.justice.gov.uk>
2023-01-10 16:44:34 +0000

    bumping up version

--- src/main/kotlin/uk/gov/justice/digital/hmpps/config/ExceptionHandler.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/config/ExceptionHandler.kt
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException
 
-
 @RestControllerAdvice
 class ExceptionHandler {
   @ExceptionHandler(ValidationException::class)
