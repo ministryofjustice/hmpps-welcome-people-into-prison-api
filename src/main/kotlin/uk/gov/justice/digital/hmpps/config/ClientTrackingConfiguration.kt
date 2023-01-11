86ff3f9e0f979fb8c360413a9495e7a57277befe
Michal Nawrocki <michal.nawrocki@digital.justice.gov.uk>
2023-01-11 09:32:06 +0000

    bumping version

--- src/main/kotlin/uk/gov/justice/digital/hmpps/config/ClientTrackingConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/config/ClientTrackingConfiguration.kt
@@ -14,7 +14,7 @@ import org.springframework.web.servlet.HandlerInterceptor
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 import java.text.ParseException
-import java.util.*
+import java.util.Optional
 
 @Configuration
 @ConditionalOnExpression("T(org.apache.commons.lang3.StringUtils).isNotBlank('\${applicationinsights.connection.string:}')")
