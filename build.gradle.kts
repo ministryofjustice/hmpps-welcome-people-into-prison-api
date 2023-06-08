c75fec89885cba4587d7fa51b7b17bf973e847cd
Michal Nawrocki <michal.nawrocki@digital.justice.gov.uk>
2023-06-08 08:16:11 +0100

    DCS-2248 library upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.2.0-beta-2"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.2.0"
   kotlin("plugin.spring") version "1.8.21"
   kotlin("plugin.jpa") version "1.8.21"
 }
