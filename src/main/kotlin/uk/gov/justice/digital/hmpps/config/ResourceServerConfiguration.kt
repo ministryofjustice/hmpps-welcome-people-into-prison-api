29005089ad2f778daab946d376a3496a9b2e37be
GCheema <50441412+GurnankCheema@users.noreply.github.com>
2023-08-22 15:58:11 +0100

    MAP-262 refactor request matchers

--- src/main/kotlin/uk/gov/justice/digital/hmpps/config/ResourceServerConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/config/ResourceServerConfiguration.kt
@@ -7,13 +7,15 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(prePostEnabled = true, proxyTargetClass = true)
 class ResourceServerConfiguration {
   @Bean
-  fun securityFilterChain(http: HttpSecurity): SecurityFilterChain =
+  fun securityFilterChain(http: HttpSecurity, mvc: MvcRequestMatcher.Builder): SecurityFilterChain =
     http
       .sessionManagement { sessionManagement ->
         sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
@@ -23,9 +25,15 @@ class ResourceServerConfiguration {
       }
       .authorizeHttpRequests { auth ->
         auth.requestMatchers(
-          "/webjars/**", "/favicon.ico", "/csrf",
-          "/health/**", "/info", "/ping",
-          "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html",
+          mvc.pattern("/webjars/**"),
+          mvc.pattern("/favicon.ico"),
+          mvc.pattern("/csrf"),
+          mvc.pattern("/health/**"),
+          mvc.pattern("/info"),
+          mvc.pattern("/ping"),
+          mvc.pattern("/v3/api-docs/**"),
+          mvc.pattern("/swagger-ui/**"),
+          mvc.pattern("/swagger-ui.html"),
         )
           .permitAll().anyRequest().authenticated()
       }
@@ -37,4 +45,9 @@ class ResourceServerConfiguration {
         }
       }
       .build()
+
+  @Bean
+  fun mvc(introspector: HandlerMappingIntrospector?): MvcRequestMatcher.Builder? {
+    return MvcRequestMatcher.Builder(introspector)
+  }
 }
