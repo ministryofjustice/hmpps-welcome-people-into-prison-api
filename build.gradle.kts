c8dd9b3240e964edc01594e6ff0b2d474e925434
michalnawrockidj <92578842+michalnawrockidj@users.noreply.github.com>
2023-12-22 09:26:20 +0000

    MAP-547 security update (#234)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.9.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.11.0"
   kotlin("plugin.spring") version "1.9.20"
   kotlin("plugin.jpa") version "1.9.20"
 }
@@ -19,21 +19,21 @@ dependencies {
   implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-csv")
 
   implementation("org.apache.commons:commons-text:1.10.0")
-  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
-  implementation("commons-codec:commons-codec:1.15")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
+  implementation("commons-codec:commons-codec:1.16.0")
 
   runtimeOnly("org.flywaydb:flyway-core")
-  runtimeOnly("org.postgresql:postgresql:42.5.4")
-  testRuntimeOnly("com.h2database:h2:2.2.220")
+  runtimeOnly("org.postgresql:postgresql:42.7.1")
+  testRuntimeOnly("com.h2database:h2:2.2.224")
 
-  testImplementation("com.github.tomakehurst:wiremock-jre8-standalone:2.35.0")
+  testImplementation("org.wiremock:wiremock-standalone:3.3.1")
 
   testImplementation("org.springframework.security:spring-security-test")
-  testImplementation("com.tngtech.archunit:archunit-junit5-api:1.0.1")
-  testImplementation("com.tngtech.archunit:archunit-junit5:1.0.1")
-  testImplementation("io.jsonwebtoken:jjwt-api:0.11.5")
-  testImplementation("io.jsonwebtoken:jjwt-impl:0.11.5")
-  testImplementation("io.jsonwebtoken:jjwt-orgjson:0.11.5")
+  testImplementation("com.tngtech.archunit:archunit-junit5-api:1.2.1")
+  testImplementation("com.tngtech.archunit:archunit-junit5:1.2.1")
+  testImplementation("io.jsonwebtoken:jjwt-api:0.12.3")
+  testImplementation("io.jsonwebtoken:jjwt-impl:0.12.3")
+  testImplementation("io.jsonwebtoken:jjwt-orgjson:0.12.3")
   constraints {
     implementation("org.json:json:20230618") {
       because("previous transitive version has CVE-2022-45688")
