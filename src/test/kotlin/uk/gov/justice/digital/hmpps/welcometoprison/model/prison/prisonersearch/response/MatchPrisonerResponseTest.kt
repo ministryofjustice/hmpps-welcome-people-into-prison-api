041fbcf1d8708828ac3a8a25262a8ca1e744c1bf
marcusaleman <168440942+marcusaleman@users.noreply.github.com>
2024-08-30 13:33:27 +0100

    MAP-1460 1 MatchPrisonerResponse branch coverage testing upgrade scenarios null Kover (#251)

--- src/test/kotlin/uk/gov/justice/digital/hmpps/welcometoprison/model/prison/prisonersearch/response/MatchPrisonerResponseTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/welcometoprison/model/prison/prisonersearch/response/MatchPrisonerResponseTest.kt
@@ -15,6 +15,20 @@ class MatchPrisonerResponseTest {
     assertThat(reference.copy(status = "XXXXX").isCurrentPrisoner).isTrue
   }
 
+  @Test
+  fun `isCurrentPrisoner should return false when status is null`() {
+    val prisonerResponseWithNullStatus = reference.copy(status = null)
+
+    assertThat(prisonerResponseWithNullStatus.isCurrentPrisoner).isFalse
+  }
+
+  @Test
+  fun `isCurrentPrisoner should return false when status is INACTIVE OUT`() {
+    val prisonerResponseWithInactiveOutStatus = reference.copy(status = INACTIVE_OUT)
+
+    assertThat(prisonerResponseWithInactiveOutStatus.isCurrentPrisoner).isFalse
+  }
+
   companion object {
     val reference =
       MatchPrisonerResponse("JIM", "SMITH", LocalDate.of(1961, 4, 1), "A123AA", "123/456", "SF80/655108T", "Male", "ACTIVE IN", lastMovementTypeCode = "ADM", prisonId = "MDI")
