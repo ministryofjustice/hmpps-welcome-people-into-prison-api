2fb1fb7d6c0e1671ab5f857123b985190966ccf1
marcusaleman <marcos.aleman@digital.justice.gov.uk>
2024-09-02 15:41:54 +0100

    MAP-1460-31-PrisonApiClientTest-Telemetry PropagateClientError red coverage

--- src/test/kotlin/uk/gov/justice/digital/hmpps/welcometoprison/model/prison/PrisonApiClientTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/welcometoprison/model/prison/PrisonApiClientTest.kt
@@ -478,6 +478,28 @@ class PrisonApiClientTest {
   }
 
   @Test
+  fun `return from temporary absences fails`() {
+    val offenderNumber = "ABC123A"
+
+    mockServer.stubErrorTemporaryAbsencesSuccess(offenderNumber, 400)
+    try {
+      val response = prisonApiClient.confirmTemporaryAbsencesArrival(
+        offenderNumber,
+        TemporaryAbsencesArrival(
+          agencyId = "NMI",
+          movementReasonCode = "ET",
+          commentText = "",
+          receiveTime = LocalDateTime.of(2021, 11, 15, 1, 0, 0),
+        ),
+      )
+      assertThat(response.offenderNo).isEqualTo(offenderNumber)
+    } catch (exception: Exception) {
+    }
+
+    verify(telemetryClient).trackEvent(eq("PrisonApiClientError"), any(), eq(null))
+  }
+
+  @Test
   fun `Confirm get Agency Test`() {
     val agencyId = "AGE1"
     mockServer.stubGetAgencySuccess(agencyId)
