0b9f90bb3d394f1afc03569a65ab55acec428da4
Michal Nawrocki <michal.nawrocki@digital.justice.gov.uk>
2024-10-18 13:36:32 +0100

    Adding missing configuration

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM --platform=$BUILDPLATFORM eclipse-temurin:21-jre-jammy AS builder
+FROM --platform=$BUILDPLATFORM eclipse-temurin:21-jdk-jammy AS builder
 
 ARG BUILD_NUMBER
 ENV BUILD_NUMBER ${BUILD_NUMBER:-1_0_0}
@@ -15,7 +15,6 @@ ENV BUILD_NUMBER ${BUILD_NUMBER:-1_0_0}
 
 RUN apt-get update && \
     apt-get -y upgrade && \
-    apt-get install -y curl && \
     rm -rf /var/lib/apt/lists/*
 
 ENV TZ=Europe/London
--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,8 @@ configurations {
 
 dependencies {
   annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.0.7")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-security")
@@ -23,7 +25,6 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
   implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-csv")
-
   implementation("org.apache.commons:commons-text:1.12.0")
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
   implementation("commons-codec:commons-codec:1.16.1")
@@ -33,7 +34,12 @@ dependencies {
   runtimeOnly("org.flywaydb:flyway-database-postgresql")
   testRuntimeOnly("com.h2database:h2:2.3.230")
 
+  testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:1.0.7")
   testImplementation("org.wiremock:wiremock-standalone:3.9.1")
+  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.22") {
+    exclude(group = "io.swagger.core.v3")
+  }
+}
 
   testImplementation("org.springframework.security:spring-security-test")
   testImplementation("com.tngtech.archunit:archunit-junit5-api:1.3.0")
@@ -42,17 +48,13 @@ dependencies {
   testImplementation("io.jsonwebtoken:jjwt-impl:0.12.5")
   testImplementation("io.jsonwebtoken:jjwt-orgjson:0.12.5")
 }
-java {
-  toolchain.languageVersion = JavaLanguageVersion.of(21)
+
+kotlin {
+jvmToolchain(21)
 }
 
 tasks {
-  withType<KotlinCompile> {
-    compilerOptions {
-      apiVersion.set(KOTLIN_2_1)
-      freeCompilerArgs.set(listOf("-Xemit-jvm-type-annotations"))
-      jvmTarget.set(JvmTarget.JVM_21)
-      languageVersion.set(KOTLIN_2_1)
-    }
-  }
+withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+compilerOptions.jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21
+}
 }
